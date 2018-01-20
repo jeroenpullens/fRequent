bcd5a98888cbc897c048efbbdf2c67d04a4c71b5
Jeroen Pullens <jwmp@agro.au.dk>
2018-01-20 15:03:10 +0100

    Update R package
    Added zootodf function

--- fRequent/R/zootodf.R
+++ fRequent/R/zootodf.R
@@ -3,12 +3,14 @@
 #' @description Converting zoo-object to data frame with proper indexing of time column and column names
 #'
 #' @param value must be a zoo-object
+#' @import zoo
 #' @references Marvin Reich (2017), mreich@@posteo.de
 # ' @examples examples MISSING
+#' @export
 
 zootodf <- function(value) {
   df <- as.data.frame(value)
-  df$time <- index(value) #create a Date column
+  df$time <- zoo::index(value) #create a Date column
   rownames(df) <- NULL #so row names not filled with dates
   df <- df[,c(ncol(df), 1:(ncol(df)-1))] #reorder columns so Date first
   return(df)
